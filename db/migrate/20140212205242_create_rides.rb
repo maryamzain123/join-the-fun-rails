42b5515119e02c9f887a72d11f86965d0d6c5854
maryamzain123 <smaryam.zain@gmail.com>
2021-04-30 21:31:34 -0500

    commit tree 77de822091a471e67566c7a151f3dcba57883375

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.string :passenger_id
+      t.string :taxi_id
       t.timestamps null: false
     end
   end
